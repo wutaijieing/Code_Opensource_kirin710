fa58c9b1ca35e92e21a5299bed76747501a26995
Coconutat <coconutat@protonmail.com>
2023-04-11 17:44:38 +0800

    Clean useless file

--- kernel/build_KSU.sh
+++ kernel/build_KSU.sh
@@ -65,7 +65,6 @@ make ARCH=arm64 O=out $EV -j256
 if [ -f out/arch/arm64/boot/Image.gz ];
 then
 	echo "***Packing Kirin710 Kernel...***"
-	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01  --output Kirin710_V"$v".img
 	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01  --output Kirin710_V"$v"_PM.img
 
 	cd tools/AnyKernel3
