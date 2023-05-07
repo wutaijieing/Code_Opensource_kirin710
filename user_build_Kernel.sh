0ce9434a0e92dc471d6c2cab0c68ed7098b375e2
Coconutat <coconutat@protonmail.com>
2023-05-07 23:57:16 +0800

    Fix build Scripts

--- user_build_Kernel.sh
+++ user_build_Kernel.sh
@@ -43,9 +43,9 @@ if [ -f out/arch/arm64/boot/Image.gz ];
 then
 	echo "***Packing Kirin710 version kernel...***"
 
-	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz -base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01 --output Kirin710_V"$v"_9.0.img
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01 --output Kirin710_V"$v"_9.0.img
 
-	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz -base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01 --output Kirin710_V"$v"_9.0_PM.img
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01 --output Kirin710_V"$v"_9.0_PM.img
 	
 	cp out/arch/arm64/boot/Image.gz Image.gz 
 
